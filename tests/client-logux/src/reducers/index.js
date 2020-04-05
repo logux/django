7da9e9adcbaed0d9fca9f9d3e8391a5c9743f7ba
egregors <root@egregors.com>
2020-04-05 15:49:34 +1000

    Logux client (0.7.0) for test app

---
+++ tests/client-logux/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+    // TODO: Add reducers depends on application purposes
+    test: (state = 0) => state // Remove me when you will have real reducer
+})
