dc35abe9cedc8e09d612b14628a1ee5d3a9a9dd4
egregors <root@egregors.com>
2020-04-10 16:00:18 +1000

    Test case helper

--- tests/test_app/tests.py
+++ tests/test_app/tests.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict, Any
 
 from django.conf import settings
 from django.http import JsonResponse
@@ -6,7 +7,18 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-class ProxyAuthTestCase(TestCase):
+class LoguxTestCase(TestCase):
+    """ TestCase helper. Easy way to make Logux protocol requests """
+
+    def logux_request(self, data: Dict[str, Any]) -> JsonResponse:
+        return json.loads(self.client.post(
+            path=reverse('logux-dispatch'),
+            data=data,
+            content_type='application/json'
+        ).content.decode('utf-8'))
+
+
+class ProxyAuthTestCase(LoguxTestCase):
     """ Logux Proxy server and Django backend communication """
 
     def setUp(self) -> None:
@@ -14,43 +26,35 @@ class ProxyAuthTestCase(TestCase):
         settings.LOGUX_AUTH_FUNC = lambda user_id, token: token == self.good_token
 
     def test_proxy_auth_good_secret(self) -> None:
-        r: JsonResponse = json.loads(self.client.post(
-            path=reverse('logux-dispatch'),
-            data={
-                "version": 2,
-                "password": "secret",
-                "commands": [
-                    [
-                        "auth",
-                        "38",
-                        "good-token",
-                        "gf4Ygi6grYZYDH5Z2BsoR"
-                    ]
+        r: JsonResponse = self.logux_request({
+            "version": 2,
+            "password": "secret",
+            "commands": [
+                [
+                    "auth",
+                    "38",
+                    "good-token",
+                    "gf4Ygi6grYZYDH5Z2BsoR"
                 ]
-            },
-            content_type='application/json'
-        ).content.decode('utf-8'))
+            ]
+        })
 
         self.assertEqual(r[0][0], 'authenticated')
         self.assertEqual(r[0][1], 'gf4Ygi6grYZYDH5Z2BsoR')
 
     def test_proxy_auth_bad_secret(self) -> None:
-        r: JsonResponse = json.loads(self.client.post(
-            path=reverse('logux-dispatch'),
-            data={
-                "version": 2,
-                "password": "wrong-secret",
-                "commands": [
-                    [
-                        "auth",
-                        "38",
-                        "good-token",
-                        "gf4Ygi6grYZYDH5Z2BsoR"
-                    ]
+        r: JsonResponse = self.logux_request({
+            "version": 2,
+            "password": "wrong-secret",
+            "commands": [
+                [
+                    "auth",
+                    "38",
+                    "good-token",
+                    "gf4Ygi6grYZYDH5Z2BsoR"
                 ]
-            },
-            content_type='application/json'
-        ).content.decode('utf-8'))
+            ]
+        })
 
         self.assertEqual(r[0][0], 'error')
         self.assertEqual(r[0][1], 'Unauthorised Logux proxy server')
@@ -62,7 +66,7 @@ class WrongLoguxCommandTypeTestCase(TestCase):
     pass
 
 
-class LoguxAuthCommandTestCase(TestCase):
+class LoguxAuthCommandTestCase(LoguxTestCase):
     """ Auth command """
 
     def setUp(self) -> None:
@@ -71,72 +75,60 @@ class LoguxAuthCommandTestCase(TestCase):
         settings.LOGUX_AUTH_FUNC = lambda user_id, token: token == self.good_token and user_id == self.good_user_id
 
     def test_success_auth(self) -> None:
-        r: JsonResponse = json.loads(self.client.post(
-            path=reverse('logux-dispatch'),
-            data={
-                "version": 2,
-                "password": "secret",
-                "commands": [
-                    [
-                        "auth",
-                        "42",
-                        self.good_token,
-                        "gf4Ygi6grYZYDH5Z2BsoR"
-                    ]
+        r: JsonResponse = self.logux_request({
+            "version": 2,
+            "password": "secret",
+            "commands": [
+                [
+                    "auth",
+                    "42",
+                    self.good_token,
+                    "gf4Ygi6grYZYDH5Z2BsoR"
                 ]
-            },
-            content_type='application/json'
-        ).content.decode('utf-8'))
+            ]
+        })
 
         self.assertEqual(r[0][0], 'authenticated')
         self.assertEqual(r[0][1], 'gf4Ygi6grYZYDH5Z2BsoR')
 
     def test_denied_auth(self):
-        r: JsonResponse = json.loads(self.client.post(
-            path=reverse('logux-dispatch'),
-            data={
-                "version": 2,
-                "password": "secret",
-                "commands": [
-                    [
-                        "auth",
-                        "69",
-                        "wrong-token",
-                        "gf4Ygi6grYZYDH5Z2BsoR"
-                    ]
+        r: JsonResponse = self.logux_request({
+            "version": 2,
+            "password": "secret",
+            "commands": [
+                [
+                    "auth",
+                    "69",
+                    "wrong-token",
+                    "gf4Ygi6grYZYDH5Z2BsoR"
                 ]
-            },
-            content_type='application/json'
-        ).content.decode('utf-8'))
+            ]
+        })
 
         self.assertEqual(r[0][0], 'denied')
         self.assertEqual(r[0][1], 'gf4Ygi6grYZYDH5Z2BsoR')
 
 
-class LoguxServerErrorsTestCase(TestCase):
+class LoguxServerErrorsTestCase(LoguxTestCase):
 
     def test_unknown_action(self):
-        r: JsonResponse = json.loads(self.client.post(
-            path=reverse('logux-dispatch'),
-            data={
-                "version": 2,
-                "password": "secret",
-                "commands": [
-                    [
-                        "action",
-                        {
-                            "type": "user/unknown",
-                            "user": 38,
-                            "name": "New"
-                        },
-                        {
-                            "id": "1560954012838 38:Y7bysd:O0ETfc 0",
-                            "time": 1560954012838
-                        }
-                    ]
+        r: JsonResponse = self.logux_request({
+            "version": 2,
+            "password": "secret",
+            "commands": [
+                [
+                    "action",
+                    {
+                        "type": "user/unknown",
+                        "user": 38,
+                        "name": "New"
+                    },
+                    {
+                        "id": "1560954012838 38:Y7bysd:O0ETfc 0",
+                        "time": 1560954012838
+                    }
                 ]
-            },
-            content_type='application/json'
-        ).content.decode('utf-8'))
+            ]
+        })
 
         self.assertEqual(r[0], ['unknownAction', '1560954012838 38:Y7bysd:O0ETfc 0'])
