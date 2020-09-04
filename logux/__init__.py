639dd48afdb94e31468141bc60f7817af0b2719e
egregors <root@egregors.com>
2020-09-04 14:20:09 +1000

    bump: 2.0.0rc4

--- logux/__init__.py
+++ logux/__init__.py
@@ -2,7 +2,7 @@
     Django Logux integration engine: https://logux.io
 """
 __title__ = 'Django Logux integration engine'
-__version__ = "2.0.0rc3"
+__version__ = "2.0.0rc4"
 __author__ = 'Vadim Iskuchekov @egregors'
 __license__ = 'MIT License'
 
